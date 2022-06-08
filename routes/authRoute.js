2d3d74644685121f52a8bfa4d8fda597a492f07a
rajthosani <rajthosani@gmail.com>
2022-06-08 23:45:03 +0530

    index.js

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -19,7 +19,7 @@ router.post("/register", async (req, res) => {
     const user = await newUser.save();
     res.status(200).json(user);
   } catch (err) {
-    res.status(500).json(err)
+    res.status(500).json(err);
   }
 });
 
@@ -27,14 +27,14 @@ router.post("/register", async (req, res) => {
 router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ email: req.body.email });
-    !user && res.status(404).json("user not found");
+    !user && res.status(500).json("user not found");
 
-    const validPassword = await bcrypt.compare(req.body.password, user.password)
-    !validPassword && res.status(400).json("wrong password")
+    const validPassword = await bcrypt.compare(req.body.password, user.password);
+    !validPassword && res.status(500).json("wrong password");
 
-    res.status(200).json(user)
+    res.status(200).json(user);
   } catch (err) {
-    res.status(500).json(err)
+    res.status(500).json(err);
   }
 });
 
